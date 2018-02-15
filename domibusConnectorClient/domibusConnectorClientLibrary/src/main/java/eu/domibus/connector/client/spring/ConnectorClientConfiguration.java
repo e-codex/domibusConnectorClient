d79460a8eecb44a84b760f46743af02c32400aff
Stephan Spindler <stephan.spindler@extern.brz.gv.at>
2018-02-15 14:40:55 +0100

    Empty Spring ClientConfiguration

---
+++ domibusConnectorClient/domibusConnectorClientLibrary/src/main/java/eu/domibus/connector/client/spring/ConnectorClientConfiguration.java
@@ -0,0 +1,15 @@
+
+package eu.domibus.connector.client.spring;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author {@literal Stephan Spindler <stephan.spindler@extern.brz.gv.at> }
+ */
+@Configuration
+public class ConnectorClientConfiguration {
+
+    //TODO: load timer job if pull/pull
+    
+}
