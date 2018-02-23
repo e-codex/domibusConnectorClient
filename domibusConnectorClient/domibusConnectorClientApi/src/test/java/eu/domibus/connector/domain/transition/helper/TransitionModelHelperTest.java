eb157bfbd7ad84bc8c279a37a195d9ee50ecddeb
Stephan Spindler <stephan.spindler@extern.brz.gv.at>
2018-02-23 16:49:05 +0100

    Helper methods for transition model added

---
+++ domibusConnectorClient/domibusConnectorClientApi/src/test/java/eu/domibus/connector/domain/transition/helper/TransitionModelHelperTest.java
@@ -0,0 +1,30 @@
+
+package eu.domibus.connector.domain.transition.helper;
+
+import eu.domibus.connector.domain.transition.DomibusConnectorMessageType;
+import eu.domibus.connector.domain.transition.testutil.TransitionCreator;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ *
+ * @author Stephan Spindler <stephan.spindler@extern.brz.gv.at>
+ */
+public class TransitionModelHelperTest {
+
+
+    @Test
+    public void testIsEvidenceMessage_epoMessage_shouldBeFalse() {
+        DomibusConnectorMessageType epoMessage = TransitionCreator.createEpoMessage();
+        assertThat(TransitionModelHelper.isEvidenceMessage(epoMessage)).isFalse();
+    }
+
+    
+    @Test
+    public void testIsEvidenceMessage_nonDelivery_shouldBeTrue() {
+        DomibusConnectorMessageType nonDeliveryMessage = TransitionCreator.createEvidenceNonDeliveryMessage();
+        assertThat(TransitionModelHelper.isEvidenceMessage(nonDeliveryMessage)).isTrue();        
+    }
+    
+    
+}
