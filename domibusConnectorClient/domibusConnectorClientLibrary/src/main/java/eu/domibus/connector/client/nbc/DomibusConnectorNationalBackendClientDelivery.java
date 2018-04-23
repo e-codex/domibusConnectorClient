7caf11c773ed741fa37dc961883e38ee8a2d3c11
Stephan Spindler <stephan.spindler@extern.brz.gv.at>
2018-04-23 15:39:27 +0200

    removed unnecessary libraryApi package

--- domibusConnectorClient/domibusConnectorClientLibrary/src/main/java/eu/domibus/connector/client/nbc/DomibusConnectorNationalBackendClientDelivery.java
+++ domibusConnectorClient/domibusConnectorClientLibrary/src/main/java/eu/domibus/connector/client/nbc/DomibusConnectorNationalBackendClientDelivery.java
@@ -4,7 +4,6 @@ import eu.domibus.connector.client.exception.ImplementationMissingException;
 import eu.domibus.connector.client.exception.DomibusConnectorNationalBackendClientException;
 import eu.domibus.connector.domain.transition.DomibusConnectorMessageType;
 
-import java.util.List;
 
 public interface DomibusConnectorNationalBackendClientDelivery {
 
