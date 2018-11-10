34ffdd7a710f8cde326d5c78528a981b302b130e
Stephan Spindler <stephan.spindler@extern.brz.gv.at>
2018-11-10 15:40:09 +0100

    commit work

--- domibusConnectorClient/domibusConnectorClientWebUi/src/main/java/eu/domibus/connector/client/configuration/ConnectorClientRestWebConfiguration.java
+++ domibusConnectorClient/domibusConnectorClientWebUi/src/main/java/eu/domibus/connector/client/configuration/ConnectorClientRestWebConfiguration.java
@@ -5,10 +5,12 @@ import eu.domibus.connector.client.storage.entity.EntityClassesPackage;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.ws.config.annotation.EnableWs;
 
 @Configuration
 @EnableJpaRepositories(basePackageClasses = RepoPackage.class)
 @EntityScan(basePackageClasses = EntityClassesPackage.class)
+@EnableWs
 public class ConnectorClientRestWebConfiguration {
 
 }
