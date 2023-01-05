085971ae8ebeea0af463b23620a30968d989c5f7
Stephan Spindler <stephan.spindler@brz.gv.at>
2023-01-06 00:34:18 +0100

    use http://localhost:<server.port>/ as default for restClient

--- domibusConnectorClient/domibusConnectorClientApplication/domibusConnectorClientUI/src/main/java/eu/domibus/connector/client/ui/configuration/VaadinConnectorClientUIConfiguration.java
+++ domibusConnectorClient/domibusConnectorClientApplication/domibusConnectorClientUI/src/main/java/eu/domibus/connector/client/ui/configuration/VaadinConnectorClientUIConfiguration.java
@@ -1,5 +1,6 @@
 package eu.domibus.connector.client.ui.configuration;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +11,15 @@ public class VaadinConnectorClientUIConfiguration {
 	
 	@Value("${connector-client-rest-url}")
 	private String connectorClientRestURL;
+
+	@Value("${server.port}")
+	private int serverPort;
 	
 	@Bean
 	public WebClient restClient(WebClient.Builder builder) {
+		if (serverPort > 1024 && StringUtils.isEmpty(connectorClientRestURL)) {
+			return builder.baseUrl("http://localhost:" + serverPort + "/").build();
+		}
 		return builder.baseUrl(connectorClientRestURL).build();
 	}
 }
